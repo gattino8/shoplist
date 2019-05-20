3755efe56fdfe5f8258a259f12bde84d66cee8bc
DANA <gattino8@gmail.com>
2019-05-20 12:56:01 +0300

    /

--- src/com/javaguru/shoppinglist/Product.java
+++ src/com/javaguru/shoppinglist/Product.java
@@ -6,7 +6,7 @@ public class Product {
 
     private Long id;
     private String name;
-    private BigDecimal price;//comment
+    private BigDecimal price;
 
     public Long getId() {
         return id;
